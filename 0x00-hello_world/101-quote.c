beead44ad9786ca69838498b8e24102cdd82bc01
Ryan Tsihugwa <tsihugwa2018@gmail.com>
2023-07-12 22:24:07 -0700

    A very happy ALX Student

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,8 @@
-#include <unistd.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+exit(1);
 }
